5224967356384c9c43ed810821bdf29f7ae9919f
rahim-mustafo-x <rahim.mustafo.x@gmail.com>
2026-08-03 16:04:53 +0500

    created second service

---
+++ src/main/java/org/safa/maintenanceservice/MaintenanceServiceImageServiceApplication.java
@@ -0,0 +1,13 @@
+package org.safa.maintenanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceServiceImageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenanceServiceImageServiceApplication.class, args);
+    }
+
+}
